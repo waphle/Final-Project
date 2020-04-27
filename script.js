2dbf196c5544f1bb7debf4a20880e72ddea817df
Jeff Zhang <jz1766american.edu>
2020-04-27 12:53:48 -0400

    dumb easter egg

--- script.js
+++ script.js
@@ -4,10 +4,10 @@
 //   document.getElementById("demo").innerHTML = x;
 // }
 
-// Timer alert message for email
-function emailFunction() {
-  setTimeout(function(){ alert('To copy my email address, left click the link and choose "Copy email address."'); }, 2000); // 2000 equals to 2 seconds
-}
+// // Timer alert message for email
+// function emailFunction() {
+//   setTimeout(function(){ alert('To copy my email address, left click the link and choose "Copy email address."'); }, 2000); // 2000 equals to 2 seconds
+// }
 
 // HTML Linking functions
 function homeLink() {
