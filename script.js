8c7469f8741ef83168cb667b0821a26e176db054
Jeff Zhang <jz1766american.edu>
2020-04-29 21:13:42 -0400

    Updated

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// // Portrait url
-// function myFunction() {
-//   var x = document.getElementById("mainPageImg").src;
-//   document.getElementById("demo").innerHTML = x;
-// }
-
-// // Timer alert message for email
-// function emailFunction() {
-//   setTimeout(function(){ alert('To copy my email address, left click the link and choose "Copy email address."'); }, 2000); // 2000 equals to 2 seconds
-// }
-
 // HTML Linking functions
 function homeLink() {
   location.replace("index.html")
