d704d5323f9e0f0ae4926c3718ba1634024d8cd7
Nate <nr7195a@student.american.edu>
2020-04-30 15:56:07 -0400

    changes

--- signin.php
+++ signin.php
@@ -20,12 +20,12 @@
 
 				$username = $_SESSION["Username"];
 
-				echo  "<div class="login">$username <p>Logged In</p></div>" ;
+				echo  "<div class='login'>$username <p>Logged In</p></div>";
 
 			}
 			else {
 
-				echo "<div class="login"><p> Not Logged In </p></div>";
+				echo "<div class='login'><p> Not Logged In </p></div>";
 
 			}
 	?>
