b06e79802363f644607b10ce9c1bc6fa84e6ccf7
Jeff Zhang <jz1766american.edu>
2020-04-29 21:30:18 -0400

    Create new PHP files

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang = "en">
+
+  <head>
+    <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8"/>
+      <title> Contact Us | Epic Gaming Tutorials </title>
+      <link rel="stylesheet" type="text/css" href="index.css"/>
+      <link rel="icon" href="images/icon.png"/>
+      <script async src="script.js"></script>
+  </head>
+
+  <body>
+    <h1 id="header"> <b> Contact Us </b> </h1>
+    <p id="headerDesc"> Have questions? Found a game-breaking bug? Let us know! </p>
+
+    <div class="flexContainer">
+      <div id="infoBox">
+        <div class="row" id="home">
+          <span onClick="homeLink()"> Home </span>
+        </div>
+
+        <div class="row" id="tutorials">
+          <span onClick="tutorialLink()"> Tutorials </span>
+        </div>
+
+        <div class="row" id="testimonials">
+          <span onClick="testimonialsLink()"> Testimonials </span>
+        </div>
+
+        <div class="row" id="signin">
+          <span onClick="signinLink()"> Sign-in/up </span>
+        </div>
+
+      </div>
+    </div>
+
+  </body>
+
+</html>
