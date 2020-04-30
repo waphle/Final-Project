68d8ea0ac7d63a62f5959b781cd3b213d66b3cc7
Nate <nr7195a@student.american.edu>
2020-04-30 15:56:49 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -62,7 +62,7 @@
 		
 		}else {
 		
-			echo '<div class="login"><form action="login.inc.php" method="post"><input type="text" name="emailUsername" placeholder="Username/E-mail..."><input type="password" name="password" placeholder="Password"><button type="submit" name="login-submit">Login</button></form>;<a href="signup.php">Signup</a></div>';
+			echo '<div class="login"><form action="login.inc.php" method="post"><input type="text" name="emailUsername" placeholder="Username/E-mail..."><input type="password" name="password" placeholder="Password"><button type="submit" name="login-submit">Login</button></form><a href="signup.php">Signup</a></div>';
 		
 		}
 		?>
